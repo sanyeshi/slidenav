44e8d5ae3c137c92c06ab0293363e31ef8dbe477
孙硕磊 <sunshuolei@gmail.com>
2016-02-06 10:56:37 +0800

    Initial Commit

---
+++ SSLSlideNav/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SSLSlideNav
+//
+//  Created by 孙硕磊 on 16/2/6.
+//  Copyright © 2016年 dhu.cst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SSLSlideNavTests/SSLSlideNavTests.m
@@ -0,0 +1,39 @@
+//
+//  SSLSlideNavTests.m
+//  SSLSlideNavTests
+//
+//  Created by 孙硕磊 on 16/2/6.
+//  Copyright © 2016年 dhu.cst. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SSLSlideNavTests : XCTestCase
+
+@end
+
+@implementation SSLSlideNavTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
